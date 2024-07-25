3187aece9993ad1674a0199e0b6f88bc49112216
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-07-25 09:34:19 +0000

    new changs updated

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Navbar from '../Navbar';
+
+
+function Home() {
+  return (
+    <>
+     <Navbar></Navbar>
+      <HeroSection />
+      <Cards />
+    
+    </>
+  );
+}
+
+export default Home;
