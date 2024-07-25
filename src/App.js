f102ea7133ace1a7cb7cf67468204326b0bfa46c
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-07-25 10:16:45 +0000

    new changes added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './components/pages/Home';
-
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 const router = createBrowserRouter([
   {
       path : '/',
       element : <Home></Home>
-  }
+  },
+  {
+    path : '/services',
+    element : <Services></Services>
+},
+{
+  path : '/products',
+  element : <Products></Products>
+},
+{
+  path : '/sign-up',
+  element : <SignUp></SignUp>
+}
 ])
 function App() {
   return (
