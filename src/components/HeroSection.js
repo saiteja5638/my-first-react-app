0996670dee7fa35856b43c6b126dde5c3d276453
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-07-26 13:36:13 +0000

    new changes added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
+// import { Button } from './Button';
 import './HeroSection.css';
+import { useNavigate } from 'react-router-dom';
 
 function HeroSection() {
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate('/services');
+  };
+  
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>Let's Learn</h1>
       <p>Empower your future with our cutting-edge , designed to unlock your full potential.</p>
       <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+        <button
+        className='Try_demo_button'
+        onClick={
+          handleRedirect
+        }
         >
-          GET STARTED
-        </Button>
-        {/* <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button> */}
+          Try Demo
+        </button>
+        <button className='Premium_btn' >
+          Premium
+        </button>
       </div>
     </div>
   );
